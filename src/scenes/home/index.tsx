d308a7f36402932cd118e015cb3f0f23e26def72
Miluckshan J <52714282+miluckshan-j@users.noreply.github.com>
2021-01-27 09:48:33 +0530

    initial commit

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const Home = () => {
+  const [text, setText] = useState('');
+  const handleChange = (e : string) : any => {
+    setText(e);
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+      ['link', 'image'],
+      ['clean'],
+    ],
+  };
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image',
+  ];
+
+  return (
+    <ReactQuill
+      modules={modules}
+      formats={formats}
+      value={text}
+      onChange={handleChange}
+    />
+  );
+};
+
+export default Home;
